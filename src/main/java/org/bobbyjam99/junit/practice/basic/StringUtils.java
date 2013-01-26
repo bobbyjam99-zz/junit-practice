f617b337d547654b6e6571742fc0dac3be268e77
Tomokazu IMAI <bobbyjam99@gmail.com>
2013-01-26 11:41:58 +0900

    refactaring to toSnakeCode method.

--- src/main/java/org/bobbyjam99/junit/practice/basic/StringUtils.java
+++ src/main/java/org/bobbyjam99/junit/practice/basic/StringUtils.java
@@ -5,13 +5,18 @@ public class StringUtils {
 	/**
 	 * 文字列をスネークケース(すべて小文字で単語区切りがアンダースコア)に変換する.
 	 * 
-	 * @param string 変換する文字列
+	 * @param text
+	 *            変換する文字列
 	 * @return スネークケースに変換された文字列
 	 */
-	public static String toSnakeCase(String string) {
-		String[] splitStr = string.split("(?<=[a-z])(?=[A-Z])");
+	public static String toSnakeCase(String text) {
+
+		if (text == null)
+			throw new NullPointerException("text == null.");
+
+		String[] splitStr = text.split("(?<=[a-z])(?=[A-Z])");
 		StringBuffer sb = new StringBuffer();
-		for(String str : splitStr) {
+		for (String str : splitStr) {
 			sb.append(str.toLowerCase());
 			sb.append("_");
 		}

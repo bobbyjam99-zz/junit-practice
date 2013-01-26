73fdd1d35db1d472f35e537e61be7536d323f061
Tomokazu IMAI <bobbyjam99@gmail.com>
2013-01-26 11:35:07 +0900

    add JavaDoc

--- src/main/java/org/bobbyjam99/junit/practice/basic/StringUtils.java
+++ src/main/java/org/bobbyjam99/junit/practice/basic/StringUtils.java
@@ -2,6 +2,12 @@ package org.bobbyjam99.junit.practice.basic;
 
 public class StringUtils {
 
+	/**
+	 * 文字列をスネークケース(すべて小文字で単語区切りがアンダースコア)に変換する.
+	 * 
+	 * @param string 変換する文字列
+	 * @return スネークケースに変換された文字列
+	 */
 	public static String toSnakeCase(String string) {
 		String[] splitStr = string.split("(?<=[a-z])(?=[A-Z])");
 		StringBuffer sb = new StringBuffer();

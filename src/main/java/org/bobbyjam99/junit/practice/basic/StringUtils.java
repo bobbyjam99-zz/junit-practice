794e880767e014ec68e68b249300120ac4960f5d
Tomokazu IMAI <bobbyjam99@gmail.com>
2013-01-26 11:33:13 +0900

    implement to sucess new testcode.

--- src/main/java/org/bobbyjam99/junit/practice/basic/StringUtils.java
+++ src/main/java/org/bobbyjam99/junit/practice/basic/StringUtils.java
@@ -3,7 +3,14 @@ package org.bobbyjam99.junit.practice.basic;
 public class StringUtils {
 
 	public static String toSnakeCase(String string) {
-		return "aaa";
+		String[] splitStr = string.split("(?<=[a-z])(?=[A-Z])");
+		StringBuffer sb = new StringBuffer();
+		for(String str : splitStr) {
+			sb.append(str.toLowerCase());
+			sb.append("_");
+		}
+		sb.deleteCharAt(sb.lastIndexOf("_"));
+		return sb.toString();
 	}
 
 }
